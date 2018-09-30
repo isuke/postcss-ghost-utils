303765375d029554b7e1869d135796a380643df5
isuke <isuke770@gmail.com>
2018-09-30 21:43:36 +0900

    refactor: refactor index.js

--- index.js
+++ index.js
@@ -1,71 +1,35 @@
 const postcss = require('postcss')
 
-const allButtons = require('./lib/all-buttons')
-const borderBottomRadius = require('./lib/border-bottom-radius')
-const borderLeftRadius = require('./lib/border-left-radius')
-const borderLeftRight = require('./lib/border-left-right')
-const borderRightRadius = require('./lib/border-right-radius')
-const borderTopBottom = require('./lib/border-top-bottom')
-const borderTopRadius = require('./lib/border-top-radius')
-const circle = require('./lib/circle')
-const fontSizeLineHeight = require('./lib/font-size-line-height')
-const marginLeftRight = require('./lib/margin-left-right')
-const marginTopBottom = require('./lib/margin-top-bottom')
-const paddingLeftRight = require('./lib/padding-left-right')
-const paddingTopBottom = require('./lib/padding-top-bottom')
-const size = require('./lib/size')
+const rules = [
+  { name: 'all-buttons', func: require('./lib/all-buttons') },
+  { name: 'border-bottom-radius', func: require('./lib/border-bottom-radius') },
+  { name: 'border-left-radius', func: require('./lib/border-left-radius') },
+  { name: 'border-left-right', func: require('./lib/border-left-right') },
+  { name: 'border-right-radius', func: require('./lib/border-right-radius') },
+  { name: 'border-top-bottom', func: require('./lib/border-top-bottom') },
+  { name: 'border-top-radius', func: require('./lib/border-top-radius') },
+  { name: 'circle', func: require('./lib/circle') },
+  { name: 'font-size-line-height', func: require('./lib/font-size-line-height') },
+  { name: 'margin-left-right', func: require('./lib/margin-left-right') },
+  { name: 'margin-top-bottom', func: require('./lib/margin-top-bottom') },
+  { name: 'padding-left-right', func: require('./lib/padding-left-right') },
+  { name: 'padding-top-bottom', func: require('./lib/padding-top-bottom') },
+  { name: 'size', func: require('./lib/size') },
+]
 
 module.exports = postcss.plugin('postcss-ghost-utils', (_opts) => (root, _result) => {
   root.walkAtRules('ghost', (util) => {
     const name = util.params.split(/\(/, 1)[0].trim()
     const args = util.params.replace(new RegExp(`^${name}\\((.+)\\)`), '$1').split(/\s*,\s*/)
 
-    switch (name) {
-      case 'font-size-line-height':
-        fontSizeLineHeight(util, args)
-        break
-      case 'margin-top-bottom':
-        marginTopBottom(util, args)
-        break
-      case 'margin-left-right':
-        marginLeftRight(util, args)
-        break
-      case 'padding-top-bottom':
-        paddingTopBottom(util, args)
-        break
-      case 'padding-left-right':
-        paddingLeftRight(util, args)
-        break
-      case 'border-top-bottom':
-        borderTopBottom(util, args)
-        break
-      case 'border-left-right':
-        borderLeftRight(util, args)
-        break
-      case 'size':
-        size(util, args)
-        break
-      case 'border-top-radius':
-        borderTopRadius(util, args)
-        break
-      case 'border-bottom-radius':
-        borderBottomRadius(util, args)
-        break
-      case 'border-left-radius':
-        borderLeftRadius(util, args)
-        break
-      case 'border-right-radius':
-        borderRightRadius(util, args)
-        break
-      case 'circle':
-        circle(util, args)
-        break
-      case 'all-buttons':
-        allButtons(util, args)
-        break
-      default:
-        // TODO: implement error handling
-        break
+    const rule = rules.find((rule) => {
+      return rule.name === name
+    })
+
+    if (rule) {
+      rule.func(util, args)
+    } else {
+      // TODO: implement error handling
     }
   })
 })
